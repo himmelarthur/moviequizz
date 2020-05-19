9348e0f222b1abcd9c0b46e8f8b24453d2cd5e3f
Arthur Himmel <himmel.arthur@gmail.com>
2020-05-19 11:34:56 +0200

    Timer

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Game = () => {
-    return <div>Game</div>
+    const [secondsElapsed, setSecondsElapsed] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setSecondsElapsed(secondsElapsed + 1);
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [secondsElapsed, setSecondsElapsed]);
+
+    return <div><h1>Game</h1><div>{secondsElapsed} seconds elapsed</div></div>
 }
 
 export default Game;
